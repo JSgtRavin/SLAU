128f8747b31cb1db8f8797146989f7c1a4d2a89a
Mr_Ga1ahad <gr_ku.ba@mail.ru>
2025-09-30 14:46:17 +0700

    WIP

--- SLAU.m
+++ SLAU.m
@@ -1,21 +1,21 @@
 clc
 clear
 clearAllMemoizedCaches
-size = input("Введите необходимую размерность матрицы коэффициентов: ");
-fprintf("Размерность матрицы коэффициентов: %d\n", size);
-vectors = input("Введите необходимуе количество векторов свободных членов: ");
+size_ = input("Введите необходимую размерность матрицы коэффициентов: ");
+fprintf("Размерность матрицы коэффициентов: %d\n", size_);
+vectors = input("Введите необходимое количество векторов свободных членов: ");
 fprintf("Количество векторов свободных членов: %d\n", vectors);
 
-A = rand(size, size)*100
-b = rand(vectors, size)*100
+A = rand(size_, size_)*100
+b = rand(vectors, size_)*100
 fprintf("Выберите метод решения. [GS] для метода Гаусса или [LU] для LU-разложения \n");
-choise = 0;
-while choise == 0
+choice = 0;
+while choice == 0
   method = input("Введите код метода: ", 's');
   if method == "GS"
-    choise = 1;
+    choice = 1;
 elseif method == "LU"
-    choise = 1;
+    choice = 1;
 else
     disp("Неправильно выбран метод")
 end
@@ -24,12 +24,33 @@ if method == "GS"
     disp("Вы выбрали метода Гаусса")
     tic
     result = _gauss_ (A, b);
+    disp(result);
     elapsed_time = toc;
 
 elseif method == "LU"
     disp("Вы выбрали метода LU-разложения")
     tic
-    result = _lu_ (A, b)
+    result = _lu_ (A, b);
+    disp(result);
     elapsed_time = toc;
-    disp("elapsed_time")
-end
+
+endif
+fprintf("elapsed_time: %d\n", elapsed_time);
+
+% Проверка A * X = b
+disp("Проверка A * X = b:");
+for i = 1:vectors
+    computed_b = A * result(i,:)';
+    original_b = b(i,:)';
+    disp(original_b);
+    disp(" ");
+    disp(computed_b);
+    error = norm(computed_b - original_b);
+    fprintf("Вектор свободных членов %d: норма ошибки = %e\n", i, error);
+    if error < 1e-6
+        disp("Проверка пройдена: A * X ≈ b (в пределах погрешности).");
+    else
+        disp("Проверка не пройдена: A * X ≠ b.");
+    endif
+endfor
+
